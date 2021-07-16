1cf0070fe545954de3d62c2c6d638084e829187c
Alexandre Macabies <web+oss@zopieux.com>
2021-07-17 01:44:27 +0200

    Implement an iframe fallback for Chrome for some mimetypes.
    Also, prefer mimetype from URL if available. Otherwise guess mime.

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,47 @@
 import encrypt from 'browser-encrypt-attachment';
 import { guessmime } from './guessmime';
 
-function decryptFileFromUrl(emxc_url) {
-    var https_url = new URL(emxc_url.replace("emxc", "https"));
+async function decryptFileFromUrl(emxc_url) {
+    const httpsUrl = new URL(emxc_url.replace("emxc", "https"));
 
-    var key = https_url.searchParams.get("key")
-    var iv = https_url.searchParams.get("iv")
-    var hash = https_url.searchParams.get("hash")
+    const key = httpsUrl.searchParams.get("key");
+    const iv = httpsUrl.searchParams.get("iv");
+    const hash = httpsUrl.searchParams.get("hash");
+    const mimeFromUrl = httpsUrl.searchParams.get("mimetype");
 
-    var keyObj = {
+    const keyObj = {
         "kty": "oct",
         "key_ops": [
-          "decrypt", "encrypt"
+            "decrypt", "encrypt"
         ],
         "k": key,
         "alg": "A256CTR"
+    };
+    const info = { key: keyObj, iv, hashes: { sha256: hash } };
+    httpsUrl.search = "";
+
+    const response = await fetch(httpsUrl);
+    const buffer = await response.arrayBuffer();
+    const data = await encrypt.decryptAttachment(buffer, info);
+    const type = mimeFromUrl || guessmime(data);
+    const blob = new Blob([data], { type });
+    const href = URL.createObjectURL(blob);
+
+    const kBrokenChromeMimes = /^video\//;
+    if (!!window.chrome && type.match(kBrokenChromeMimes)) {
+        // Chrome is dumb as a brick and garbage-collects the blob/ref before it can be displayed on reload.
+        // Use an iframe instead.
+        console.log("Falling back to an iframe for", href);
+        document.write(`
+            <style>body, html { margin: 0; padding: 0; }</style>
+            <iframe src="${href}" frameborder="0" style="width:100%; height: 100%"></iframe>
+        `);
+    } else {
+        window.location.href = href;
     }
-
-    var info = {key: keyObj, iv: iv, hashes: {sha256: hash}};
-    https_url.search = "";
-
-    var req = new Request(https_url);
-    return fetch(req).then(function(response) {
-        return response.arrayBuffer();
-    }).then(function(buffer) {
-        return encrypt.decryptAttachment(buffer, info);
-    }).then((data) => {
-        // console.log(guessmime(data));
-        var blob = new Blob([data], {type: guessmime(data)});
-        var blob_url = URL.createObjectURL(blob);
-        window.location.href = blob_url;
-    });
 }
 
 if (window.location.hash) {
-    var emxc_url = window.location.hash.substring(1);
-    decryptFileFromUrl(emxc_url);
+    const emxcUrl = window.location.hash.substring(1);
+    decryptFileFromUrl(emxcUrl);
 }
